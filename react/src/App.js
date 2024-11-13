222997082ac6c0be14bb9ffbf3192edb48621d0a
Ishan Royyuru <142559396+ishanroyyuru@users.noreply.github.com>
2024-11-12 20:02:23 -0800

    Changed text

--- react/src/App.js
+++ react/src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        Full stack template!
+        This is a change
       </header>
     </div>
   );
